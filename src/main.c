57f5ca6911e179266ba055ced77b81486fb22349
Matteo Romani <kimosabe.mr@gmail.com>
2025-03-26 23:36:26 +0100

    undrstand array of pointers + casting in calloc (size_t)

--- src/main.c
+++ src/main.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
+typedef {
+    char* key;
+    char* value;
+} Item;
+
+typedef {
+    int size;
+    int count;
+    Item** items;
+} HashTable;
+
+Item createNewItem(char* k, char* v){
+    Item i = malloc(sizeof(Item));
+    i->key = k;
+    i->value = v;
+    return i;
+}
+
+HashTable createNewHashTable(int s){
+    HashTable* ht = malloc(sizeof(HashTable));
+
+    ht->size = s;
+    ht->count = 0;
+    ht-> items = calloc((size_t)HashTable->size, sizeof(Item*));
+    return ht;
+}
+
+
 int main(){
     printf("Hash table");
 }
